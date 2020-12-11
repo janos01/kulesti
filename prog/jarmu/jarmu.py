8f63214a8a9f656e696e533d89f0b9869c0ec36e
Sallai András <termih@gmail.com>
2020-12-11 07:05:19 +0100

    Python fájlkezelés, Zindex, Bootstrap

---
+++ prog/jarmu/jarmu.py
@@ -0,0 +1,22 @@
+
+class Jarmu:
+	rendszam = 'ismeretlen'
+	szin = 'ismeretlen'
+	kerkekAllpota = []
+
+kocsi1 = Jarmu()
+ladacska = Jarmu()
+	
+kocsi1.rendszam = 'ABC-123'
+kocsi1.szin = 'piros'
+
+ladacska.rendszam = 'AAA-111'
+ladacska.szin = 'fehér'
+ladacska.maxseb = 200
+
+print(kocsi1.rendszam)
+print(kocsi1.szin)
+print(ladacska.maxseb)
+
+
+
